2e363fa4098b769a6fa772e58ca5d10f39252252
Stephanie <yuhezhangyanru@sina.com>
2018-08-04 23:37:26 +0800

    modify data format

---
+++ CrawlBlogTrafficDataTool/CrawlBlogTrafficDataTool/KeyValueInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// date：2018-8-3 22:48:31
+/// function：键值对信息
+/// </summary>
+public class KeyValueInfo
+{
+    public string key;
+    public string value;
+
+    public KeyValueInfo(string key, string value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CrawlBlogTrafficData
 CrawlBlogTrafficData for CSDN blog, and my blog website is https://blog.csdn.net/Stephanie_1
+
+备注：
+
+其中运行exe之后：
+
+博客统计参数的关键字存放在“yanruCSDNVisitLog统计关键字‘文件中，
+
+其所有关键字对应的值，存放在文件”yanruCSDNVisitLog统计关键字的值“
